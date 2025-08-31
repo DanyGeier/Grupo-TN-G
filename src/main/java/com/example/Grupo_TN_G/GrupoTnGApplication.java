d90dd58e15db7062ea2be4301c9461bdb99656d7
Daniel <31143269+DanyGeier@users.noreply.github.com>
2025-08-31 16:55:54 -0300

    agrego base de proyecto spring

---
+++ src/main/java/com/example/Grupo_TN_G/GrupoTnGApplication.java
@@ -0,0 +1,13 @@
+package com.example.Grupo_TN_G;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoTnGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoTnGApplication.class, args);
+	}
+
+}
