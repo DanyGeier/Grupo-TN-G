30e54fa71c32c32d52a1461a2d88cce1413e5a57
Martin Bombara <martinbombara97@gmail.com>
2025-09-11 22:32:58 -0300

    conexion con base de datos funcionando

--- backend/users/src/main/java/com/grupog/grpc/service/UsuarioGrpcService.java
+++ backend/users/src/main/java/com/grupog/grpc/service/UsuarioGrpcService.java
@@ -1,18 +1,27 @@
 package com.grupog.grpc.service;
 
+import java.util.stream.Collectors;
+
 import com.grupog.ListaUsuariosResponse;
 import com.grupog.ListarUsuariosRequest;
 import com.grupog.Usuario;
 import com.grupog.UsuarioServiceGrpc.UsuarioServiceImplBase;
 import com.grupog.entities.UsuarioEntity;
+import com.grupog.repositories.UsuarioRepository;
 
 import io.grpc.stub.StreamObserver;
 //import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
-public class UsuarioGrpcService extends UsuarioServiceImplBase{
-	
+public class UsuarioGrpcService extends UsuarioServiceImplBase {
+
+	private final UsuarioRepository usuarioRepository;
+
+	public UsuarioGrpcService(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
 	private Usuario toProto(UsuarioEntity e) {
 		return Usuario.newBuilder()
 				.setId(e.getIdUsuario())
@@ -20,10 +29,13 @@ public class UsuarioGrpcService extends UsuarioServiceImplBase{
 				.setEmail(e.getEmail())
 				.build();
 	}
-	
+
 	@Override
 	public void listarUsuarios(ListarUsuariosRequest request, StreamObserver<ListaUsuariosResponse> responseObserver) {
-		ListaUsuariosResponse response = ListaUsuariosResponse.newBuilder().addUsuarios(Usuario.newBuilder().setEmail("test@test.com").build()).build();
+		Usuario usuario = usuarioRepository.findAll()
+				.stream()
+				.map(this::toProto).collect(Collectors.toList()).get(0);
+		ListaUsuariosResponse response = ListaUsuariosResponse.newBuilder().addUsuarios(usuario).build();
 		responseObserver.onNext(response);
 		responseObserver.onCompleted();
 	}
