25bb44a0f0904203b893c5e5ac894bc731be8ca3
Martin Bombara <martinbombara97@gmail.com>
2025-09-18 00:07:09 -0300

    encriptar password y guardar en db

--- backend/users/src/main/java/com/grupog/grpc/service/UsuarioGrpcService.java
+++ backend/users/src/main/java/com/grupog/grpc/service/UsuarioGrpcService.java
@@ -5,21 +5,25 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.grupog.ActualizarUsuarioRequest;
 import com.grupog.BuscarUsuarioPorIdRequest;
 import com.grupog.CrearUsuarioRequest;
 import com.grupog.EstadoUsuario;
 import com.grupog.ListaUsuariosResponse;
 import com.grupog.ListarUsuariosRequest;
-import com.grupog.LoginData;
+import com.grupog.LoginRequest;
 import com.grupog.Rol;
 import com.grupog.Usuario;
 import com.grupog.UsuarioServiceGrpc.UsuarioServiceImplBase;
 import com.grupog.entities.RolEntity;
 import com.grupog.entities.UsuarioEntity;
+import com.grupog.mappers.UsuarioMapper;
 import com.grupog.RespuestaExito;
 import com.grupog.repositories.RolRepository;
 import com.grupog.repositories.UsuarioRepository;
+import com.grupog.utils.PasswordGenerator;
 
 import io.grpc.stub.StreamObserver;
 //import lombok.RequiredArgsConstructor;
@@ -30,56 +34,22 @@ public class UsuarioGrpcService extends UsuarioServiceImplBase {
 
 	private final UsuarioRepository usuarioRepository;
 	private final RolRepository rolRepository;
+	private final UsuarioMapper usuarioMapper;
+	private final PasswordEncoder passwordEncoder;
 
-	public UsuarioGrpcService(UsuarioRepository usuarioRepository, RolRepository rolRepository) {
+	public UsuarioGrpcService(UsuarioRepository usuarioRepository, RolRepository rolRepository,
+			PasswordEncoder passwordEncoder) {
 		this.usuarioRepository = usuarioRepository;
 		this.rolRepository = rolRepository;
-	}
-
-	private Usuario toProto(UsuarioEntity e) {
-		return Usuario.newBuilder()
-				.setId(e.getIdUsuario())
-				.setNombreUsuario(e.getNombreUsuario())
-				.setNombre(e.getNombre())
-				.setApellido(e.getApellido())
-				.setTelefono(e.getTelefono() != null ? e.getTelefono() : "")
-				.setEmail(e.getEmail())
-				.setRol(mapRolEntityToProto(e.getRol()))
-				.setEstado(e.isActivo() ? EstadoUsuario.ACTIVO : EstadoUsuario.INACTIVO)
-				.setFechaCreacion(e.getFechaCreacion() != null ? e.getFechaCreacion().toEpochMilli()
-						: Instant.now().toEpochMilli())
-				.build();
-	}
-
-	private UsuarioEntity toEntity(CrearUsuarioRequest request) {
-		UsuarioEntity entity = new UsuarioEntity();
-		entity.setNombreUsuario(request.getNombreUsuario());
-		entity.setNombre(request.getNombre());
-		entity.setApellido(request.getApellido());
-		entity.setTelefono(request.getTelefono());
-		entity.setEmail(request.getEmail());
-		entity.setClave("changeme"); // TODO Ver clave
-		entity.setActivo(true);
-		entity.setRol(mapRolProtoToEntity(request.getRol()));
-		entity.setFechaCreacion(Instant.now());
-		return entity;
-	}
-
-	private Rol mapRolEntityToProto(RolEntity rolEntity) {
-		String nombre = rolEntity.getAuthority().replace("ROLE_", "");
-		return Rol.valueOf(nombre);
-	}
-
-	private RolEntity mapRolProtoToEntity(Rol rol) {
-		return rolRepository.findByNombreRol(rol.name())
-				.orElseThrow(() -> new IllegalStateException("Rol inválido: " + rol.name()));
+		this.usuarioMapper = new UsuarioMapper(rolRepository);
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public void listarUsuarios(ListarUsuariosRequest request, StreamObserver<ListaUsuariosResponse> responseObserver) {
 		List<Usuario> usuarios = usuarioRepository.findAll()
 				.stream()
-				.map(this::toProto).collect(Collectors.toList());
+				.map(this.usuarioMapper::toProto).collect(Collectors.toList());
 		ListaUsuariosResponse response = ListaUsuariosResponse.newBuilder().addAllUsuarios(usuarios).build();
 		responseObserver.onNext(response);
 		responseObserver.onCompleted();
@@ -97,9 +67,15 @@ public class UsuarioGrpcService extends UsuarioServiceImplBase {
 			return;
 		}
 
-		UsuarioEntity entity = toEntity(request);
+		UsuarioEntity entity = usuarioMapper.toEntity(request);
+
+		String password = PasswordGenerator.generarPassword();
+
+		entity.setClave(passwordEncoder.encode(password));
 		UsuarioEntity guardado = usuarioRepository.save(entity);
-		Usuario usuario = toProto(guardado);
+		Usuario usuario = usuarioMapper.toProto(guardado);
+
+		// Envio de email con el password
 		responseObserver.onNext(usuario);
 		responseObserver.onCompleted();
 	}
@@ -108,7 +84,7 @@ public class UsuarioGrpcService extends UsuarioServiceImplBase {
 	public void buscarUsuarioPorId(BuscarUsuarioPorIdRequest request, StreamObserver<Usuario> responseObserver) {
 		UsuarioEntity entity = usuarioRepository.findById(request.getId())
 				.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-		Usuario usuario = toProto(entity);
+		Usuario usuario = usuarioMapper.toProto(entity);
 		responseObserver.onNext(usuario);
 		responseObserver.onCompleted();
 	}
@@ -133,27 +109,11 @@ public class UsuarioGrpcService extends UsuarioServiceImplBase {
 		entity.setApellido(request.getApellido());
 		entity.setTelefono(request.getTelefono());
 		entity.setEmail(request.getEmail());
-		entity.setRol(mapRolProtoToEntity(request.getRol()));
+		entity.setRol(usuarioMapper.mapRolProtoToEntity(request.getRol()));
 		entity.setActivo(request.getEstado() == EstadoUsuario.ACTIVO);
 
 		UsuarioEntity guardado = usuarioRepository.save(entity);
-		Usuario usuario = toProto(guardado);
-		responseObserver.onNext(usuario);
-		responseObserver.onCompleted();
-	}
-
-	@Override
-	public void autenticarUsuario(LoginData request, StreamObserver<Usuario> responseObserver) {
-		UsuarioEntity entity = usuarioRepository.findByNombreUsuarioAndActivoTrue(request.getNombreUsuario())
-				.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-
-		// TODO ver tema clave encriptada
-		if (!request.getClave().equals(entity.getClave())) {
-			responseObserver.onError(new RuntimeException("Credenciales incorrectas"));
-			return;
-		}
-
-		Usuario usuario = toProto(entity);
+		Usuario usuario = usuarioMapper.toProto(guardado);
 		responseObserver.onNext(usuario);
 		responseObserver.onCompleted();
 	}
